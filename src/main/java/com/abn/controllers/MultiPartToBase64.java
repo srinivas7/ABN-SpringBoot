18511caab8979337b8266f2f594e6d556e607917
Srinivas <Srinivas@192.168.0.2>
2018-03-19 17:50:00 +0530

    generating base64 image

--- src/main/java/com/abn/controllers/MultiPartToBase64.java
+++ src/main/java/com/abn/controllers/MultiPartToBase64.java
@@ -10,13 +10,14 @@ public class MultiPartToBase64 {
 	String img;
 	
 	public String convertToBase64(MultipartFile file) {
-		String base64Delimiter = ",";
+		String base64Prefix = "data:image/png;base64,";
 		StringBuilder sb = new StringBuilder();
 		sb.append("data:image/png;base64,");
 		Encoder en = Base64.getEncoder();
 		
 		try {
 			img = en.encodeToString(file.getBytes());
+			img = sb+img;
 			System.out.println("image url is.."+ img);
 		} catch (IOException e) {
 			System.out.println("can't convert to image Base64 URL");
